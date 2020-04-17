4cf2ababe3f2126675623e031dd37b39cdd78a94
Jonathan Robbins <jonathan.robbins@rcn.org.uk>
2020-04-17 18:38:03 +0100

    use of slots

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,31 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-               <app-quote>
-                   <h2>The quote</h2>
-                   <p>A wonderful quote</p>
-               </app-quote>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <app-quote>
+          <h2 slot="title">{{ quoteTitle }}</h2>
+          <p slot="content">A wonderful quote</p>
+        </app-quote>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import Quote from './components/Quote.vue'
-    export default {
-        components: {
-            appQuote: Quote
-        }
-    }
+import Quote from "./components/Quote.vue";
+
+export default {
+  data() {
+    return {
+        quoteTitle: 'The Quote'
+    };
+  },
+  components: {
+    appQuote: Quote,
+  },
+};
 </script>
 
 <style>
+
 </style>
